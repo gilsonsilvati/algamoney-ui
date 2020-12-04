0f97b4d4dd10620ac59290d6db977b2aa1cacdff
Gilson Silva <gilsonsilvati@gmail.com>
2020-12-04 18:26:12 -0300

    Adicionando tipo campo valor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { InputTextareaModule } from 'primeng/inputtextarea';
 import { CalendarModule } from 'primeng/calendar';
 import { SelectButtonModule } from 'primeng/selectbutton';
 import { DropdownModule } from 'primeng/dropdown';
+import { InputNumberModule } from 'primeng/inputnumber';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -35,7 +36,8 @@ import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-ca
         InputTextareaModule,
         CalendarModule,
         SelectButtonModule,
-        DropdownModule
+        DropdownModule,
+        InputNumberModule
     ],
     providers: [],
     bootstrap: [AppComponent]
