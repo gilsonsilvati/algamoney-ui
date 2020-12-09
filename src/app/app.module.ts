8ed343be5d9a4146ac95f1e10c79d0cb1c48199e
Gilson Silva <gilsonsilvati@gmail.com>
2020-12-09 20:00:00 -0300

    Desafio: criando o feature module de pessoas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,52 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
-
-import { InputTextModule } from 'primeng/inputtext';
-import { ButtonModule } from 'primeng/button';
-import { TableModule } from 'primeng/table';
-import { TooltipModule } from 'primeng/tooltip';
-import { InputTextareaModule } from 'primeng/inputtextarea';
-import { CalendarModule } from 'primeng/calendar';
-import { SelectButtonModule } from 'primeng/selectbutton';
-import { DropdownModule } from 'primeng/dropdown';
-import { InputMaskModule } from 'primeng/inputmask';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
-import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
 import { MessageComponent } from './message/message.component';
-import { PessoasGridComponent } from './pessoas-grid/pessoas-grid.component';
 
 import { LancamentosModule } from './lancamentos/lancamentos.module';
+import { PessoasModule } from './pessoas/pessoas.module';
 
 @NgModule({
     declarations: [
         AppComponent,
         NavbarComponent,
-        PessoasPesquisaComponent,
-        PessoaCadastroComponent,
         MessageComponent,
-        PessoasGridComponent
     ],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
-        FormsModule,
 
         LancamentosModule,
-
-        InputTextModule,
-        ButtonModule,
-        TableModule,
-        TooltipModule,
-        InputTextareaModule,
-        CalendarModule,
-        SelectButtonModule,
-        DropdownModule,
-        InputMaskModule
+        PessoasModule
     ],
     providers: [],
     bootstrap: [AppComponent]
