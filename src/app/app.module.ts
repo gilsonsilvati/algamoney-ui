c436fd30c5d37e23bd59f68170064dfa294d1028
Gilson Silva <gilsonsilvati@gmail.com>
2020-12-09 20:52:21 -0300

    Desafio: criando o Core Module NavBarComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+
+import { CoreModule } from './core/core.module';
 
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
 
 @NgModule({
     declarations: [
-        AppComponent,
-        NavbarComponent
+        AppComponent
     ],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
+
+        CoreModule,
 
         LancamentosModule,
         PessoasModule
