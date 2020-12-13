d37c8712e58488a7287feee5aad5484687fcc470
Gilson Silva <gilsonsilvati@gmail.com>
2020-12-13 15:37:40 -0300

    Configurando a paginação lazy do PrimeNG

--- src/app/lancamentos/lancamento.service.ts
+++ src/app/lancamentos/lancamento.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
-import { LancamentoFiltro } from './LancamentoFiltro';
+import { LancamentoFiltro } from './lancamentos-pesquisa/LancamentoFiltro';
 import * as moment from 'moment';
 
 @Injectable({
@@ -15,7 +15,7 @@ export class LancamentoService {
 
     pesquisar(filtro: LancamentoFiltro): Promise<any> {
         const headers = new HttpHeaders()
-            .append('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJhZG1pbkBhbGdhbW9uZXkuY29tIiwic2NvcGUiOlsicmVhZCIsIndoaXRlIl0sIm5vbWUiOiJBZG1pbmlzdHJhZG9yIiwiZXhwIjoxNjA3ODcyMTA2LCJhdXRob3JpdGllcyI6WyJST0xFX0NBREFTVFJBUl9DQVRFR09SSUEiLCJST0xFX1BFU1FVSVNBUl9QRVNTT0EiLCJST0xFX1JFTU9WRVJfUEVTU09BIiwiUk9MRV9DQURBU1RSQVJfTEFOQ0FNRU5UTyIsIlJPTEVfUEVTUVVJU0FSX0xBTkNBTUVOVE8iLCJST0xFX1JFTU9WRVJfTEFOQ0FNRU5UTyIsIlJPTEVfQ0FEQVNUUkFSX1BFU1NPQSIsIlJPTEVfUEVTUVVJU0FSX0NBVEVHT1JJQSJdLCJqdGkiOiJyNzQxNkhPTVhmUzlVeW5Qb1RMaS1mZkpWeVkiLCJjbGllbnRfaWQiOiJhbmd1bGFyIn0.akWCkWnuB0ZSQ2PZiXa759yy-KwdE9dQRO-vpJQiJg8');
+            .append('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJhZG1pbkBhbGdhbW9uZXkuY29tIiwic2NvcGUiOlsicmVhZCIsIndoaXRlIl0sIm5vbWUiOiJBZG1pbmlzdHJhZG9yIiwiZXhwIjoxNjA3ODg1NzgyLCJhdXRob3JpdGllcyI6WyJST0xFX0NBREFTVFJBUl9DQVRFR09SSUEiLCJST0xFX1BFU1FVSVNBUl9QRVNTT0EiLCJST0xFX1JFTU9WRVJfUEVTU09BIiwiUk9MRV9DQURBU1RSQVJfTEFOQ0FNRU5UTyIsIlJPTEVfUEVTUVVJU0FSX0xBTkNBTUVOVE8iLCJST0xFX1JFTU9WRVJfTEFOQ0FNRU5UTyIsIlJPTEVfQ0FEQVNUUkFSX1BFU1NPQSIsIlJPTEVfUEVTUVVJU0FSX0NBVEVHT1JJQSJdLCJqdGkiOiI0QUgtY2NPRFhXNERzZ1VjLU5rOTdmY1hmdXMiLCJjbGllbnRfaWQiOiJhbmd1bGFyIn0.xNY43G7P5bpFi5WrE724HCoHNo0G7KLGr3OmXDZNN8I');
 
         let params = new HttpParams()
             .set('page', filtro.pagina.toString())
--- src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -1,27 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { LazyLoadEvent } from 'primeng/api';
 
 import { LancamentoService } from '../lancamento.service';
-import { LancamentoFiltro } from './../LancamentoFiltro';
+import { LancamentoFiltro } from './LancamentoFiltro';
 
 @Component({
     selector: 'app-lancamentos-pesquisa',
     templateUrl: './lancamentos-pesquisa.component.html'
 })
-export class LancamentosPesquisaComponent implements OnInit {
+export class LancamentosPesquisaComponent {
 
     filtro: LancamentoFiltro = new LancamentoFiltro();
-
     lancamentos: any[] = [];
+    totalRegistros = 0;
+    loading: boolean;
 
     constructor(private lancamentoService: LancamentoService) { }
 
-    ngOnInit(): void {
-        this.pesquisar();
+    pesquisar(pagina: number = 0): void {
+        this.filtro.pagina = pagina;
+
+        this.lancamentoService.pesquisar(this.filtro)
+            .then(resultado => {
+                this.lancamentos = resultado.lancamentos;
+                this.totalRegistros = resultado.total;
+            });
     }
 
-    pesquisar(): void {
-        this.lancamentoService.pesquisar(this.filtro)
-            .then(resultado => this.lancamentos = resultado.lancamentos);
+    aoMudarPagina(event: LazyLoadEvent): void {
+        this.loading = true;
+        const pagina: number = event.first / event.rows;
+
+        setTimeout(() => {
+            this.pesquisar(pagina);
+            this.loading = false;
+        }, 1000);
     }
 
 }
